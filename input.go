682e36ec743d01fee6a732ba8fc8dd2ee67cd601
Stuart Scott <stuart.murray.scott@gmail.com>
2021-05-12 08:50:49 -0700

    Incorporating feedback

--- input.go
+++ input.go
@@ -1,29 +1,18 @@
 package shaping
 
-import "image/color"
+import (
+	"golang.org/x/image/font"
+	"golang.org/x/image/math/fixed"
+)
 
 type Input interface {
 	// Text returns the characters to be shaped.
 	Text() []rune
-	// Color returns the color to render the text in.
-	Color() color.Color
-
-	// TODO should these be wrapped in an interface, eg. Font() image/font?
-
-	// FontFamily returns the name of the font family, eg. Helvetica.
-	FontFamily() string
-	// FontFace returns the name of the font face, eg. Bold Italic.
-	// TODO would it be better to split these out into Bold() bool, Italic() bool, Monospace() bool, etc?
-	FontFace() string
-	// FontSize returns the size of the font, eg. 14.
+	// Direction returns the directionality of the text.
+	Direction() Direction
+	// Face returns the font face to render the text in.
+	Face() font.Face
+	// Size returns the size of the font, eg. 14.
 	// TODO is this a scaled value, exact pixels, or display dependand?
-	FontSize() int
-
-	// TODO should these be wrapped in an interface, eg Script() Script, where Script has enums Regular, Super, and Sub?
-	Superscript() bool
-	Subscript() bool
-
-	// TODO are these useful?
-	Language() string
-	Locale() string
+	Size() fixed.Int26_6
 }
--- output.go
+++ output.go
@@ -1,12 +1,17 @@
 package shaping
 
-import "image"
+import (
+	"golang.org/x/image/math/fixed"
+	"image"
+)
 
 type Output interface {
 	// Advance returns the distance the Dot has advanced.
-	Advance() int
+	Advance() fixed.Int26_6
+	// Baseline returns the distance the baseline is from the top.
+	Baseline() fixed.Int26_6
 	// Bounds returns the smallest rectangle capable of containing the shaped text.
-	Bounds() image.Rectangle
+	Bounds() fixed.Rectangle26_6
 	// Length returns the number of glyphs in the output.
 	Length() int
 	// Glyph returns the glyph at the given index.
