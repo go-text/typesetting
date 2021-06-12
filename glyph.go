7a2a996761bd6721f1f4605673460da55c4fad3e
Stuart Scott <stuart.murray.scott@gmail.com>
2021-06-12 15:25:37 -0700

    Add Advance to Glyph

--- glyph.go
+++ glyph.go
@@ -3,6 +3,8 @@ package shaping
 import "golang.org/x/image/font/sfnt"
 
 type Glyph interface {
+	// Advance returns the distance the Dot has advanced.
+	Advance() fixed.Int26_6
 	// Segments returns the vector segments that describe the shape of the shaped text.
 	Segments() sfnt.Segments
 }
