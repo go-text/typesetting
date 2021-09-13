21ef07ba153bf1531668f06fa22245c87864de9f
Benoit KUGLER <x.ben.x@free.fr>
2021-09-13 19:48:11 +0200

    Add context information into input runes (#4)
    * Add context information into input runes
    
    * Flip convention and update documentation

--- input.go
+++ input.go
@@ -7,8 +7,13 @@ import (
 )
 
 type Input interface {
-	// Text returns the characters to be shaped.
-	Text() []rune
+	// Text returns the characters to be shaped, which is `text[start:end]`.
+	//
+	// When shaping part of a larger text (e.g. a run of text from a line),
+	// instead of passing just the substring corresponding to the run, it is preferable to pass the whole
+	// line and specify the run `start` and `end` to give the shaper the full context to be able,
+	// for example, to do cross-run Arabic shaping or properly handle combining marks at start of run.
+	Text() (text []rune, start, end int)
 	// Direction returns the directionality of the text.
 	Direction() di.Direction
 	// Face returns the font face to render the text in.
