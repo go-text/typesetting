a5c4fc42e7170ab3ab274ecda560e19e69912f5b
Benoit KUGLER <x.ben.x@free.fr>
2021-12-10 12:56:51 +0100

    Add glyph lookup support (#3)
    * add nominal glyph method
    
    * use type alias for fonts.GID

--- face.go
+++ face.go
@@ -1,3 +1,14 @@
 package font
 
-type Face interface{}
+import "github.com/benoitkugler/textlayout/fonts"
+
+// make sure that we can use textlayout/fonts.Face as Face
+var _ Face = (fonts.Face)(nil)
+
+type GID = fonts.GID
+
+type Face interface {
+	// NominalGlyph returns the glyph identifier used to represent the given rune,
+	// or false the rune is not supported by the font.
+	NominalGlyph(r rune) (GID, bool)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/go-text/font
 
 go 1.15
+
+require github.com/benoitkugler/textlayout v0.0.3
