667e4030ab68e196510a48e62939080598fdf54e
Chris Waldon <christopher.waldon.dev@gmail.com>
2021-12-14 14:13:10 -0500

    di: define vertical directions and axis helper
    This commit introduces constants for vertical text layout and
    a helper for determining the layout axis of text.
    
    Signed-off-by: Chris Waldon <christopher.waldon.dev@gmail.com>

--- di/direction.go
+++ di/direction.go
@@ -1,10 +1,33 @@
 package di
 
-type Direction int
+// Direction indicates the layout direction of a piece of text.
+type Direction uint8
 
 const (
+	// DirectionLTR is for Left-to-Right text.
 	DirectionLTR Direction = iota
+	// DirectionRTL is for Right-to-Left text.
 	DirectionRTL
+	// DirectionTTB is for Top-to-Bottom text.
+	DirectionTTB
+	// DirectionBTT is for Bottom-to-Top text.
+	DirectionBTT
+)
 
-// Could add Top-to-Bottom (TTB) and Bottom-to-Top (BTT) in future.
+// Axis returns the layout axis for d.
+func (d Direction) Axis() Axis {
+	switch d {
+	case DirectionBTT, DirectionTTB:
+		return Vertical
+	default:
+		return Horizontal
+	}
+}
+
+// Axis indicates the axis of layout for a piece of text.
+type Axis uint8
+
+const (
+	Horizontal Axis = iota
+	Vertical
 )
