dfbf8a306d54f530dddafde8d3c1a0f1c7c0df95
Andy Williams <andy@andy.xyz>
2021-12-16 11:59:04 -0800

    simplify interface definition

--- font/face.go
+++ font/face.go
@@ -5,13 +5,6 @@ import (
 	"github.com/benoitkugler/textlayout/harfbuzz"
 )
 
+type Face = fonts.Face
 type GID = fonts.GID
 type GlyphMask = harfbuzz.GlyphMask
-
-type Face interface {
-	fonts.Face // make sure that we can use textlayout/fonts.Face as Face
-
-	// NominalGlyph returns the glyph identifier used to represent the given rune,
-	// or false the rune is not supported by the font.
-	NominalGlyph(r rune) (GID, bool)
-}
