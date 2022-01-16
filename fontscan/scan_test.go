e07cf52f883de467574eb5a8c5615c52769ae96e
Benoit KUGLER <x.ben.x@free.fr>
2022-01-16 20:10:04 +0100

    initial implementation of footprint scan

--- fontscan/scan_test.go
+++ fontscan/scan_test.go
@@ -15,10 +15,35 @@ func TestScanFamilies(t *testing.T) {
 		log.Fatal(err)
 	}
 
+	// simulate a duplicate directory entry
+	directories = append(directories, directories...)
+
 	got, err := ScanFamilies(directories...)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Printf("Found %d fonts in %s\n", len(got), time.Since(ti))
+}
+
+func TestScanFonts(t *testing.T) {
+	ti := time.Now()
+
+	directories, err := DefaultFontDirs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fontset, err := ScanFonts(directories...)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Show some basic stats
+	repartition := map[Format]int{}
+	for _, font := range fontset {
+		repartition[font.Format]++
+	}
+
+	fmt.Printf("Found %d fonts in %s (repartition: %v)\n", len(fontset), time.Since(ti), repartition)
 }
