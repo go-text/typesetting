eb07b80842426b2b486ff2b355be8c03637adc00
Benoit KUGLER <x.ben.x@free.fr>
2022-01-23 16:47:59 +0100

    add style matching

--- fontscan/locate_test.go
+++ fontscan/locate_test.go
@@ -1,14 +1,26 @@
 package fontscan
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/benoitkugler/textlayout/fonts"
+)
 
 func Test_FindFont(t *testing.T) {
 	for _, family := range [...]string{
 		"arial", "times", "deja vu",
 	} {
-		_, err := FindFont(family)
+		_, loc, err := FindFont(family, Aspect{})
 		if err != nil {
 			t.Fatal(err)
 		}
+		fmt.Println(loc.File)
+
+		_, loc, err = FindFont(family, Aspect{Style: fonts.StyleItalic})
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Println(loc.File)
 	}
 }
