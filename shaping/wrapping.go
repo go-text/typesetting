24d190d3ecf139c907976ae494cdad4f938aaaf7
Chris Waldon <christopher.waldon.dev@gmail.com>
2022-10-06 13:06:49 -0400

    shaping: implement iterator wrap API
    This commit creates a stand-alone function that can be used with a
    state object to iteratively wrap a paragraph of text. This API allows
    varying with width of the paragraph on a per-line basis (for use-cases
    like inline images) and allows the caller to stop line-wrapping as soon
    as they have all of the vertical text they need (for use-cases like
    displaying the beginning of a paragraph with the rest off-screen).
    
    Signed-off-by: Chris Waldon <christopher.waldon.dev@gmail.com>

--- shaping/wrapping.go
+++ shaping/wrapping.go
@@ -221,7 +221,7 @@ func NewLineWrapper(text []rune, glyphRuns ...Output) LineWrapper {
 //
 // This function returns both a valid Output broken at b and a boolean
 // indicating whether the returned output should be used.
-func (sp LineWrapper) shouldKeepSegmentOnLine(run Output, mapping []int, lineStartRune int, b breakOption, curLineWidth, curLineUsed, nextLineWidth length) (candidateLine Output, keep, more bool) {
+func (sp LineWrapper) shouldKeepSegmentOnLine(run Output, mapping []int, lineStartRune int, b breakOption, curLineWidth, curLineUsed, nextLineWidth length) (candidateLine Output, fits, incomplete, consumed bool) {
 	// Convert the break target to an inclusive index.
 	runeStart := lineStartRune - run.Runes.Offset
 	runeEnd := b.breakAtRune - run.Runes.Offset
@@ -236,7 +236,7 @@ func (sp LineWrapper) shouldKeepSegmentOnLine(run Output, mapping []int, lineSta
 		runeEnd = len(mapping) - 1
 		// Set the more return value to true, indicating that subsequent runs should be
 		// appended to the line candidate.
-		more = true
+		incomplete = true
 	}
 	glyphStart, glyphEnd := inclusiveGlyphRange(run.Direction, runeStart, runeEnd, mapping, len(run.Glyphs))
 
@@ -247,18 +247,174 @@ func (sp LineWrapper) shouldKeepSegmentOnLine(run Output, mapping []int, lineSta
 	candidateLine.Runes.Offset = run.Runes.Offset + runeStart
 	candidateLine.Runes.Count = runeEnd - runeStart + 1
 	candidateAdvance := candidateLine.Advance.Ceil()
+
+	candidateConsumed := candidateLine.Runes.Offset+candidateLine.Runes.Count == run.Runes.Offset+run.Runes.Count
 	if candidateAdvance > curLineWidth && candidateAdvance-curLineUsed <= nextLineWidth {
 		// If it fits on the next line, put it there.
-		return candidateLine, false, more
+		return candidateLine, false, incomplete, candidateConsumed
 	}
 
-	return candidateLine, true, more
+	return candidateLine, true, incomplete, candidateConsumed
 }
 
 // nextValidBreak returns the next line-breaking candidate position if there is one.
 // If ok is false, there are no more candidates.
 func (sp LineWrapper) nextValidBreak(run Output, mapping []int) (_ breakOption, ok bool) {
 	return sp.wordBreaker.nextValid(mapping, run)
+}
+
+// BreakState holds intermediate line-wrapping state for breaking runs of text
+// across multiple lines.
+type BreakState struct {
+	breaker *breaker
+	// unusedBreak is a break requested from the breaker in a previous iteration
+	// but which was not chosen as the line ending. Subsequent invocations of
+	// WrapLine should start with this break.
+	unusedBreak breakOption
+	// isUnused indicates that the unusedBreak field is valid.
+	isUnused bool
+	// glyphRuns holds the runs of shaped text being wrapped.
+	glyphRuns []Output
+	// currentRun holds the index in use within glyphRuns.
+	currentRun int
+	// lineStartRune is the rune index of the first rune on the next line to
+	// be shaped.
+	lineStartRune int
+}
+
+// NewBreakState initializes a BreakState for the given paragraph of text.
+func NewBreakState(paragraph []rune, shapedRuns ...Output) BreakState {
+	return BreakState{
+		breaker:   newBreaker(paragraph),
+		glyphRuns: shapedRuns,
+	}
+}
+
+// WrapParagraph2 wraps the paragraph's shaped glyphs to a constant maxWidth.
+// It is equivalent to iteratively invoking WrapLine with a constant maxWidth.
+func WrapParagraph2(maxWidth int, paragraph []rune, shapedRuns ...Output) []Line {
+	state := NewBreakState(paragraph, shapedRuns...)
+	var lines []Line
+	var done bool
+	for !done {
+		var line Line
+		line, state, done = WrapLine(maxWidth, state)
+		lines = append(lines, line)
+	}
+	return lines
+}
+
+// cutRun returns the sub-run of run containing glyphs corresponding to the provided
+// _inclusive_ rune range.
+func cutRun(run Output, mapping []glyphIndex, startRune, endRune int) Output {
+	// Convert the rune range of interest into an inclusive range within the
+	// current run's runes.
+	runeStart := startRune - run.Runes.Offset
+	runeEnd := endRune - run.Runes.Offset
+	if runeStart < 0 {
+		// If the start location is prior to the run of shaped text under consideration,
+		// just work from the beginning of this run.
+		runeStart = 0
+	}
+	if runeEnd >= len(mapping) {
+		// If the break location is after the entire run of shaped text,
+		// keep through the end of the run.
+		runeEnd = len(mapping) - 1
+	}
+	glyphStart, glyphEnd := inclusiveGlyphRange(run.Direction, runeStart, runeEnd, mapping, len(run.Glyphs))
+
+	// Construct a run out of the inclusive glyph range.
+	run.Glyphs = run.Glyphs[glyphStart : glyphEnd+1]
+	run.RecomputeAdvance()
+	run.Runes.Offset = run.Runes.Offset + runeStart
+	run.Runes.Count = runeEnd - runeStart + 1
+	return run
+}
+
+// WrapLine wraps the shaped glyphs of a paragraph to a particular max width.
+// It is meant to be called iteratively to wrap each line, allowing lines to
+// be wrapped to different widths within the same paragraph. The returned
+// BreakState should always be passed as input to the next call until the
+// returned done boolean is true. Subsequent invocations with the returned
+// BreakState are invalid.
+func WrapLine(maxWidth int, state BreakState) (_ Line, _ BreakState, done bool) {
+	if len(state.glyphRuns) == 0 {
+		return nil, state, true
+	} else if len(state.glyphRuns[0].Glyphs) == 0 {
+		// Pass empty lines through as empty.
+		state.glyphRuns[0].Runes = Range{Count: state.breaker.totalRunes}
+		return Line([]Output{state.glyphRuns[0]}), state, true
+	}
+
+	lineCandidate, bestCandidate := []Output{}, []Output{}
+	candidateWidth := fixed.I(0)
+
+	for {
+		run := state.glyphRuns[state.currentRun]
+		var option breakOption
+		if state.isUnused {
+			option = state.unusedBreak
+			state.isUnused = false
+		} else {
+			var breakOk bool
+			option, breakOk = state.breaker.next()
+			if !breakOk {
+				return bestCandidate, state, true
+			}
+			state.unusedBreak = option
+		}
+		for option.breakAtRune >= run.Runes.Count+run.Runes.Offset {
+			if state.lineStartRune > run.Runes.Offset {
+				// If part of this run has already been used on a previous line, trim
+				// the runes corresponding to those glyphs off.
+				mapping := mapRunesToClusterIndices(run.Direction, run.Runes, run.Glyphs)
+				run = cutRun(run, mapping, state.lineStartRune, run.Runes.Count+run.Runes.Offset)
+			}
+			// While the run being processed doesn't contain the current line breaking
+			// candidate, just append it to the candidate line.
+			lineCandidate = append(lineCandidate, run)
+			candidateWidth += run.Advance
+			state.currentRun++
+			run = state.glyphRuns[state.currentRun]
+		}
+		mapping := mapRunesToClusterIndices(run.Direction, run.Runes, run.Glyphs)
+		if !state.breaker.isValid(option, mapping, run) {
+			// Reject invalid line break candidate and acquire a new one.
+			continue
+		}
+		candidateRun := cutRun(run, mapping, state.lineStartRune, option.breakAtRune)
+		if (candidateRun.Advance + candidateWidth).Ceil() > maxWidth {
+			// The run doesn't fit on the line.
+			if len(bestCandidate) < 1 {
+				// There is no existing candidate that fits, and we have just hit the
+				// first line breaking canddiate. Commit this break position as the
+				// best available, even though it doesn't fit.
+				lineCandidate = append(lineCandidate, candidateRun)
+				state.lineStartRune = candidateRun.Runes.Offset + candidateRun.Runes.Count
+				return lineCandidate, state, false
+			} else {
+				// The line is a valid, shorter wrapping. Return it and mark that
+				// we should reuse the current line break candidate on the next
+				// line.
+				state.isUnused = true
+				finalRunRunes := bestCandidate[len(bestCandidate)-1].Runes
+				state.lineStartRune = finalRunRunes.Count + finalRunRunes.Offset
+				return bestCandidate, state, false
+			}
+		} else {
+			// The run does fit on the line. Commit this line as the best known
+			// line, but keep lineCandidate unmodified so that later break
+			// options can be attempted to see if a more optimal solution is
+			// available.
+			if target := len(lineCandidate) + 1; cap(bestCandidate) < target {
+				bestCandidate = make([]Output, target-1, target)
+			} else if len(bestCandidate) < target {
+				bestCandidate = bestCandidate[:target-1]
+			}
+			copy(bestCandidate, lineCandidate)
+			bestCandidate = append(bestCandidate, candidateRun)
+		}
+	}
 }
 
 // WrapParagraph wraps the shaped glyphs of a paragraph to a particular max width.
@@ -282,29 +438,33 @@ func (sp LineWrapper) WrapParagraph(maxWidth int) []Line {
 	for breakOk {
 		var goodLine []Output
 		var goodLineWidth fixed.Int26_6
-		lineRunIndex := runIdx
+		goodLineRunIndex := runIdx
 		// Always keep the first segment on a line.
-		good, _, more := sp.shouldKeepSegmentOnLine(run, runeToGlyph, start, b, maxWidth, 0, maxWidth)
+		good, _, more, consumed := sp.shouldKeepSegmentOnLine(run, runeToGlyph, start, b, maxWidth, 0, maxWidth)
 		goodLine = append(goodLine, good)
 		goodLineWidth += good.Advance
 		for more {
-			lineRunIndex++
-			run = sp.glyphRuns[lineRunIndex]
+			goodLineRunIndex++
+			run = sp.glyphRuns[goodLineRunIndex]
 			runeToGlyph = mapRunesToClusterIndices(run.Direction, run.Runes, run.Glyphs)
-			good, _, more = sp.shouldKeepSegmentOnLine(run, runeToGlyph, start, b, maxWidth, goodLineWidth.Ceil(), maxWidth)
+			good, _, more, consumed = sp.shouldKeepSegmentOnLine(run, runeToGlyph, start, b, maxWidth-goodLineWidth.Ceil(), 0, maxWidth)
 			goodLine = append(goodLine, good)
 			goodLineWidth += good.Advance
+		}
+		if consumed {
+			goodLineRunIndex++
 		}
 		end := b.breakAtRune
 
 		// Search through break candidates looking for candidates that can fit on the current line.
 		for {
-			if lineRunIndex != runIdx {
+			candidateLineRunIndex := goodLineRunIndex
+			if candidateLineRunIndex != runIdx {
 				// If we've already traversed forward through the runs, reset to the beginning
 				// of the line.
 				run = sp.glyphRuns[runIdx]
 				runeToGlyph = mapRunesToClusterIndices(run.Direction, run.Runes, run.Glyphs)
-				lineRunIndex = runIdx
+				candidateLineRunIndex = runIdx
 			}
 			var candidateLine []Output
 			var candidateLineWidth fixed.Int26_6
@@ -314,21 +474,25 @@ func (sp LineWrapper) WrapParagraph(maxWidth int) []Line {
 				breakOk = false
 				break
 			}
-			candidate, ok, more := sp.shouldKeepSegmentOnLine(run, runeToGlyph, start, bb, maxWidth, goodLineWidth.Ceil(), maxWidth)
+			candidate, ok, more, candidateConsumed := sp.shouldKeepSegmentOnLine(run, runeToGlyph, start, bb, maxWidth, goodLineWidth.Ceil(), maxWidth)
 			candidateLine = append(candidateLine, candidate)
 			candidateLineWidth += candidate.Advance
 			for more && ok {
-				lineRunIndex++
-				run = sp.glyphRuns[lineRunIndex]
+				candidateLineRunIndex++
+				run = sp.glyphRuns[candidateLineRunIndex]
 				runeToGlyph = mapRunesToClusterIndices(run.Direction, run.Runes, run.Glyphs)
-				candidate, ok, more = sp.shouldKeepSegmentOnLine(run, runeToGlyph, start, bb, maxWidth, candidateLineWidth.Ceil(), maxWidth)
+				candidate, ok, more, candidateConsumed = sp.shouldKeepSegmentOnLine(run, runeToGlyph, start, bb, maxWidth-candidateLineWidth.Ceil(), 0, maxWidth)
 				candidateLine = append(candidateLine, candidate)
 				candidateLineWidth += candidate.Advance
+			}
+			if candidateConsumed {
+				candidateLineRunIndex++
 			}
 			if ok {
 				// The break described by bb fits on this line. Use this new, longer segment.
 				goodLine = candidateLine
 				goodLineWidth = candidateLineWidth
+				goodLineRunIndex = candidateLineRunIndex
 				end = bb.breakAtRune
 			} else {
 				// The break described by bb will not fit on this line, commit whatever the last good
@@ -337,6 +501,7 @@ func (sp LineWrapper) WrapParagraph(maxWidth int) []Line {
 				break
 			}
 		}
+		runIdx = goodLineRunIndex
 		outputs = append(outputs, goodLine)
 		start = end + 1
 	}
