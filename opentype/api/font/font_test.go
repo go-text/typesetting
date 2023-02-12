959574cbf2716b1bbb35b6dc9349a5388af94acf
Benoit KUGLER <benoit.kugler@gmail.com>
2023-02-12 10:39:06 +0100

    Opentype (2/2) (#42)
    * [opentype] add high level API for glyph loading and metrics access
    
    * Fix typo in opentype/api/font/font.go
    
    Co-authored-by: Sebastien Binet <binet@cern.ch>
    
    * Fix typo in opentype/api/font/font.go
    
    Co-authored-by: Sebastien Binet <binet@cern.ch>
    
    * fix typo in opentype/api/font/font.go
    
    Co-authored-by: Sebastien Binet <binet@cern.ch>
    
    * Fix typo in opentype/api/font/font.go
    
    Co-authored-by: Sebastien Binet <binet@cern.ch>
    
    * reduce stutter in PS interpreter type names
    
    * use a more standard arguments order
    
    * add license header
    
    ---------
    
    Co-authored-by: Sebastien Binet <binet@cern.ch>

---
+++ opentype/api/font/font_test.go
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: Unlicense OR BSD-3-Clause
+
+package font
+
+import (
+	"testing"
+
+	"github.com/go-text/typesetting/opentype/api"
+	tu "github.com/go-text/typesetting/opentype/testutils"
+)
+
+func TestCrashes(t *testing.T) {
+	for _, filepath := range append(tu.Filenames(t, "common"), "toys/chromacheck-svg.ttf") {
+		loadFont(t, filepath)
+	}
+}
+
+func TestGlyphName(t *testing.T) {
+	ft := loadFont(t, "toys/NamesCFF.ttf")
+	tu.Assert(t, ft.post.names == nil)
+
+	expected := [20]string{
+		".notdef", "space", "uni0622", "uni0623", "uni0624", "uni0625", "uni0626", "uni0628",
+		"uni06C0", "uni06C2", "uni06D3", "uni0625.fina", "uni0623.fina", "uni0622.fina", "uni0628.fina",
+		"uni0626.init", "uni0628.init", "uni0626.medi", "uni0628.medi", "uni06C1.fina",
+	}
+	for i, exp := range expected {
+		tu.Assert(t, ft.GlyphName(api.GID(i)) == exp)
+	}
+}
+
+func BenchmarkLoad(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, filepath := range tu.Filenames(b, "common") {
+			loadFont(b, filepath)
+		}
+	}
+}
---
+++ opentype/api/font/variations_test.go
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: Unlicense OR BSD-3-Clause
+
+package font
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/go-text/typesetting/opentype/api"
+	"github.com/go-text/typesetting/opentype/loader"
+	"github.com/go-text/typesetting/opentype/tables"
+	tu "github.com/go-text/typesetting/opentype/testutils"
+)
+
+// ported from harfbuzz/test/api/test-var-coords.c Copyright © 2019 Ebrahim Byagowi
+
+func TestVar(t *testing.T) {
+	font := loadFont(t, "toys/CFF2-VF.otf")
+
+	/* Design coords as input */
+	designCoords := []float32{206.}
+	coords := font.NormalizeVariations(designCoords)
+	tu.Assert(t, coords[0]*(1<<14) == float32(-16116.88))
+
+	// test for crash
+	for weight := float32(200); weight < 901; weight++ {
+		font.NormalizeVariations([]float32{weight})
+	}
+
+	face := Face{Font: font}
+	face.SetVariations([]Variation{{loader.MustNewTag("wght"), 206.}})
+	tu.Assert(t, len(face.Coords) == 1)
+	tu.Assert(t, face.Coords[0] == -0.9836963)
+
+	face.SetVariations(nil)
+	tu.Assert(t, len(face.Coords) == 0)
+}
+
+func TestGlyphExtentsVar(t *testing.T) {
+	font := loadFont(t, "common/SourceSans-VF-HVAR.ttf")
+
+	coords := font.NormalizeVariations([]float32{500})
+	face := Face{Font: font, Coords: coords}
+
+	ext2, _ := face.GlyphExtents(2)
+
+	tu.Assert(t, ext2 == api.GlyphExtents{XBearing: 50.192135, YBearing: 667.1601, Width: 591.8152, Height: -679.1601})
+}
+
+func TestGetDefaultCoords(t *testing.T) {
+	tf := fvar{
+		{Tag: loader.MustNewTag("wght"), Minimum: 38, Default: 88, Maximum: 250},
+		{Tag: loader.MustNewTag("wdth"), Minimum: 60, Default: 402, Maximum: 402},
+		{Tag: loader.MustNewTag("opsz"), Minimum: 10, Default: 14, Maximum: 72},
+	}
+
+	vars := []Variation{
+		{Tag: loader.MustNewTag("wdth"), Value: 60},
+	}
+	coords := tf.getDesignCoordsDefault(vars)
+	tu.Assert(t, reflect.DeepEqual(coords, []float32{88, 60, 14}))
+}
+
+func TestNormalizeVar(t *testing.T) {
+	tf := fvar{
+		{Tag: loader.MustNewTag("wdth"), Minimum: 60, Default: 402, Maximum: 500},
+	}
+
+	vars := []Variation{
+		{Tag: loader.MustNewTag("wdth"), Value: 60},
+	}
+	coords := tf.normalizeCoordinates(tf.getDesignCoordsDefault(vars))
+	tu.Assert(t, reflect.DeepEqual(coords, []float32{-1}))
+
+	vars = []Variation{
+		{Tag: loader.MustNewTag("wdth"), Value: 30},
+	}
+	coords = tf.normalizeCoordinates(tf.getDesignCoordsDefault(vars))
+	tu.Assert(t, reflect.DeepEqual(coords, []float32{-1}))
+
+	vars = []Variation{
+		{Tag: loader.MustNewTag("wdth"), Value: 700},
+	}
+	coords = tf.normalizeCoordinates(tf.getDesignCoordsDefault(vars))
+	tu.Assert(t, reflect.DeepEqual(coords, []float32{1}))
+}
+
+func TestAdvanceHVar(t *testing.T) {
+	font := loadFont(t, "common/Commissioner-VF.ttf")
+	coords := []float32{-0.4, 0, 0.8, 1}
+	// 0 - 99 GIDs
+	exps := [100]float32{
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 8, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 1.6, 1.6, 1.6, 1.6, 1.6, 1.6, 1.6, 1.6, 1.6, 1.6, 1.6, 1.6, 1.6, 1.6, 1.6, 1.6, 1.6, 1.6, 1.6,
+		1.6, 1.6, 1.6, 1.6, 1.6, 1.6, 1.6, 1.6, 3.2, 3.2, 3.2, 0, 0,
+	}
+	tu.Assert(t, font.hvar != nil)
+	for i, exp := range exps {
+		got := getAdvanceVar(font.hvar, tables.GlyphID(i), coords)
+		tu.Assert(t, exp == got)
+	}
+}
+
+func TestAdvanceNoHVar(t *testing.T) {
+	font := loadFont(t, "toys/GVAR-no-HVAR.ttf")
+
+	tu.Assert(t, len(font.fvar) == 2)
+
+	vars := []Variation{
+		{Tag: loader.MustNewTag("wght"), Value: 600},
+		{Tag: loader.MustNewTag("wght"), Value: 80},
+	}
+	face := Face{Font: font}
+	face.SetVariations(vars)
+
+	// 0 - 14 GIDs
+	exps := [15]float32{600, 1164, 1170, 813, 741, 1164, 1170, 813, 741, 270, 270, 0, 0, 0, 0}
+
+	for i, exp := range exps {
+		got := face.HorizontalAdvance(api.GID(i))
+		tu.Assert(t, exp == got)
+	}
+}
