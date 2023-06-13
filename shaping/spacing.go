b60fbbafc9cac0438d9c875c5d4b6a9332563e58
Benoit KUGLER <benoit.kugler@gmail.com>
2023-06-13 15:45:01 +0200

    [shaping] add custom word spacing

---
+++ shaping/spacing.go
@@ -0,0 +1,39 @@
+package shaping
+
+import (
+	"golang.org/x/image/math/fixed"
+)
+
+// See https://www.w3.org/TR/css-text-3/#word-separator
+func (run *Output) addWordSpacing(text []rune, additionalSpacing fixed.Int26_6) {
+	for i, g := range run.Glyphs {
+		// find the corresponding runes :
+		// to simplify, we assume the words separators are not produced by ligatures
+		// so that the cluster "rune-length" is 1
+		if g.RuneCount != 1 {
+			continue
+		}
+		r := text[g.ClusterIndex]
+		switch r {
+		case '\u0020', // space
+			'\u00A0',                   // no-break space
+			'\u1361',                   // Ethiopic word space
+			'\U00010100', '\U00010101', // Aegean word separators
+			'\U0001039F', // Ugaritic word divider
+			'\U0001091F': // Phoenician word separator
+		default:
+			continue
+		}
+		// we have a word separator: add space
+		// we do it by enlarging the separator glyph advance
+		// and distributing space around the glyph content
+		if run.Direction.IsVertical() {
+			run.Glyphs[i].YAdvance += additionalSpacing
+			run.Glyphs[i].YOffset += additionalSpacing / 2
+		} else {
+			run.Glyphs[i].XAdvance += additionalSpacing
+			run.Glyphs[i].XOffset += additionalSpacing / 2 // distribute space around the char
+		}
+	}
+	run.RecomputeAdvance()
+}
