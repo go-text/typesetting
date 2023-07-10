eb92c8acb96eb0b695cd64ab8691aac28c663dda
Benoit KUGLER <benoit.kugler@gmail.com>
2023-07-10 11:50:49 +0200

    [shaping] add custom letter spacing

--- shaping/spacing.go
+++ shaping/spacing.go
@@ -4,10 +4,11 @@ import (
 	"golang.org/x/image/math/fixed"
 )
 
-// addWordSpacing alter the run, adding [additionalSpacing] on each
+// addWordSpacing alters the run, adding [additionalSpacing] on each
 // word separator.
-// [text] is the origin input slice used to create the run.
-// See https://www.w3.org/TR/css-text-3/#word-separator
+// [text] is the input slice used to create the run.
+//
+// See also https://www.w3.org/TR/css-text-3/#word-separator
 func (run *Output) addWordSpacing(text []rune, additionalSpacing fixed.Int26_6) {
 	isVertical := run.Direction.IsVertical()
 	for i, g := range run.Glyphs {
@@ -39,5 +40,45 @@ func (run *Output) addWordSpacing(text []rune, additionalSpacing fixed.Int26_6) 
 			run.Glyphs[i].XOffset += additionalSpacing / 2 // distribute space around the char
 		}
 	}
+	run.RecomputeAdvance()
+}
+
+// addLetterSpacing alters the run, adding [additionalSpacing] between
+// each Harfbuzz clusters.
+// Space it NOT included before the first cluster or after the last.
+//
+// See also https://www.w3.org/TR/css-text-3/#letter-spacing-property
+func (run *Output) addLetterSpacing(additionalSpacing fixed.Int26_6) {
+	isVertical := run.Direction.IsVertical()
+
+	halfSpacing := additionalSpacing / 2
+	for startGIdx := 0; startGIdx < len(run.Glyphs); {
+		startGlyph := run.Glyphs[startGIdx]
+		endGIdx := startGIdx + startGlyph.GlyphCount - 1
+
+		if startGIdx > 0 {
+			if isVertical {
+				run.Glyphs[startGIdx].YAdvance += halfSpacing
+				run.Glyphs[startGIdx].YOffset += halfSpacing
+			} else {
+				run.Glyphs[startGIdx].XAdvance += halfSpacing
+				run.Glyphs[startGIdx].XOffset += halfSpacing
+			}
+		}
+
+		if endGIdx < len(run.Glyphs)-1 {
+			if isVertical {
+				run.Glyphs[endGIdx].YAdvance += halfSpacing
+				run.Glyphs[endGIdx].YOffset += halfSpacing
+			} else {
+				run.Glyphs[endGIdx].XAdvance += halfSpacing
+				run.Glyphs[endGIdx].XOffset += halfSpacing
+			}
+		}
+
+		// go to next cluster
+		startGIdx += startGlyph.GlyphCount
+	}
+
 	run.RecomputeAdvance()
 }
